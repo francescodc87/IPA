52e5315fc3a0abbb9f1ace24a85cad6c84e044b7
Francesco Del Carratore <francesco.delcarratore@manchester.ac.uk>
2019-05-07 22:19:03 +0100

    update

---
+++ R/SyntheticExperiment.R
@@ -0,0 +1,13 @@
+#' @title Database
+#'
+#' @description Dataset with the 83 simulated positive features
+#'
+#'
+#' @docType SyntheticExperiment
+#' @keywords datasets
+#' @name SyntheticExperiment
+#' @usage data(SyntheticExperiment)
+#' @format A dataset with 83 simulated features
+NULL
+
+
